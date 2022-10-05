5a848adf99c6f4d3f5c2dcd0c6b914455f078cc8
Marc Miller <marc.miller@secondfront.com>
2022-10-05 17:06:00 -0400

    update installs using go templates

--- linux-utils.sh
+++ linux-utils.sh
@@ -5,12 +5,7 @@ function main {
 }
 
 function install_package {
-  echo "Installing apt packages"
   sudo sh -c "DEBIAN_FRONTEND=noninteractive apt-get -qqy install $1" < /dev/null > /dev/null
-}
-
-function install_nix {
-  sh <(curl -L https://nixos.org/nix/install) --daemon
 }
 
 main "${@}"
